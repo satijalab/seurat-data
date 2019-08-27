84ee7bcce583e5765225dd1afbf58b42a918b37f
Paul Hoffman <mojaveazure@users.noreply.github.com>
2019-08-27 11:07:13 -0400

    Add examples

--- R/zzz.R
+++ R/zzz.R
@@ -44,6 +44,12 @@ pkg.env$attached <- vector(mode = 'character')
 #' @author Hadley Wickham
 #' @references \url{https://adv-r.hadley.nz/functions.html#missing-arguments}
 #'
+#' @examples
+#' \dontrun{
+#' 4 %||% 5
+#' NULL %|| 5
+#' }
+#'
 #' @keywords internal
 #'
 `%||%` <- function(lhs, rhs) {
@@ -133,11 +139,98 @@ AttachData <- function(pkgname = 'SeuratData') {
   invisible(x = NULL)
 }
 
+#' Enumerate a list or vector
+#'
+#' @param x A list or a vector
+#'
+#' @return A list of length \code{x} with the following named values:
+#' \describe{
+#'   \item{\code{name}}{The name of \code{x} at a given index}
+#'   \item{\code{value}}{The value of \code{x} at the corresponding index}
+#' }
+#'
+#' @note For any given index \code{i} in \code{x}, all attempts to use the name
+#' of the value of \code{x} at \code{i} will be made. If there is no name
+#' (eg. \code{nchar(x = names(x = x)[i]) == 0}), the index \code{i} will be used
+#' in its stead
+#'
+#' @keywords internal
+#'
+Enumerate <- function(x) {
+  indices <- seq_along(along.with = x)
+  keys <- names(x = x) %||% as.character(x = indices)
+  keys[nchar(x = keys) == 0] <- indices[nchar(x = keys) == 0]
+  vals <- lapply(
+    X = indices,
+    FUN = function(i) {
+      return(c('name' = keys[i], 'value' = unname(obj = x[i])))
+    }
+  )
+  return(vals)
+}
+
+#' Check to see if a matrix is empty
+#'
+#' Deterime if a matrix is empty or not
+#'
+#' @param x A matrix
+#'
+#' @return \code{TRUE} if the matrix is empty otherwise \code{FALSE}
+#'
+#' @details A matrix is considered empty if it satisfies one of the following
+#' conditions:
+#' \itemize{
+#'   \item The dimensions of the matrix are 0-by-0 (\code{all(dim(x) == 0)})
+#'   \item The dimensions of the matrix are 1-by-1 (\code{all(dim(x) == 1)}) and
+#'   the sole value is a \code{NA}
+#' }
+#' These two situations correspond to matrices generated with either
+#' \code{new('matrix')} or \code{matrix()}
+#'
+#' @examples
+#' \dontrun{
+#' IsMatrixEmpty(new('matrix'))
+#' IsMatrixEmpty(matrix())
+#' IsMatrixEmpty(matrix(1:9, nrow = 3))
+#' }
+#'
+#' @keywords internal
+#'
+IsMatrixEmpty <- function(x) {
+  matrix.dims <- dim(x = x)
+  matrix.na <- all(matrix.dims == 1) && all(is.na(x = x))
+  return(all(matrix.dims == 0) || matrix.na)
+}
+
+#' Make a space
+#'
+#' @param n Length space should be
+#'
+#' @return A space (' ') of length \code{n}
+#'
+#' @examples
+#' \dontrun{
+#' MakeSpace(10)
+#' }
+#'
+#' @keywords internal
+#'
+MakeSpace <- function(n) {
+  return(paste(rep_len(x = ' ', length.out = n), collapse = ''))
+}
+
 #' Find dataset packages based on name
 #'
 #' @param ds Names of datasets
 #'
 #' @return A vector of package names based on dataset names
+#'
+#' @examples
+#' \dontrun{
+#' NameToPackage('cbmc')
+#' NameToPackage('pbmc3k.SeuratData')
+#' NameToPackage('notadataset')
+#' }
 #'
 #' @keywords internal
 #'
